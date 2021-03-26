d51c88212dd60ea6b45d59d3dfc39a33ff1a768d
Alonso <alonso12_284@outlook.com>
2021-03-25 19:21:29 -0600

    add RWG app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+import random
 
 app = Flask(__name__)
 
@@ -6,6 +8,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route('/RandomWordGenerator', methods=['GET'])
+def RWG():
+    with open('./json/RandomWord.json') as myArr:
+        randomWordArr = json.load(myArr)
+    return render_template('randomWord.html', randomWord=random.choice(randomWordArr))
 
 
 if __name__ == '__main__':
